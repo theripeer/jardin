11d0bd26565448359e294a240d9fdc7000e11fea
Theripeer <mac@MBP-de-Mac.lan>
2024-03-25 16:52:18 -0300

    Modifocacion de model User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel; // Corrected import
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool // Corrected parameter type
+    {
+        if ($panel->getId() === 'admin') {
+            return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        }
+ 
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
